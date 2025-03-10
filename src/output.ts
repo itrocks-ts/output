a9d2490481709bde3120bf3c02770ade66d396c9
Baptiste Pillot <baptiste@pillot.fr>
2025-03-10 17:45:02 +0100

    Action icon, dynamic actions, template target

--- src/output.ts
+++ src/output.ts
@@ -1,7 +1,9 @@
 import { Action }     from '@itrocks/action'
+import { getActions } from '@itrocks/action'
 import { Need }       from '@itrocks/action'
 import { Request }    from '@itrocks/action-request'
 import { Route }      from '@itrocks/route'
+import { routeOf }    from '@itrocks/route'
 import { dataSource } from '@itrocks/storage'
 
 @Need('object', 'new')
@@ -11,7 +13,10 @@ export class Output extends Action
 
 	async html(request: Request)
 	{
-		return this.htmlTemplateResponse(await request.getObject(), request, __dirname + '/output.html')
+		const object = await request.getObject() as object // @Need('object')
+		const route  = routeOf(this)
+		this.actions = getActions(object, route.slice(route.lastIndexOf('/') + 1))
+		return this.htmlTemplateResponse(object, request, __dirname + '/output.html')
 	}
 
 	async json(request: Request)
